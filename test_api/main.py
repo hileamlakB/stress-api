c2d6594d91dee0e0a29e8a3ee7aa8cf51d628f3e
hileamlakb <ehmyitayew@college.harvard.edu>
2025-05-10 13:38:13 -0400

    setup test api

--- test_api/main.py
+++ test_api/main.py
@@ -234,4 +234,6 @@ async def cpu_load(params: LoadTestParams = Depends()):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
